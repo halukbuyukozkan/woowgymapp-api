ee855780a9f45e0bbea7a9b6279598e48fc0912e
semihbuyukozkan <semihbuyukozkan@gmail.com>
2023-04-04 15:09:44 +0300

    user(create,index,edit) pages localized

--- resources/views/user/create.blade.php
+++ resources/views/user/create.blade.php
@@ -9,7 +9,7 @@
 			<!--begin::Page title-->
 			<div data-kt-swapper="true" data-kt-swapper-mode="prepend" data-kt-swapper-parent="{default: '#kt_content_container', 'lg': '#kt_toolbar_container'}" class="page-title d-flex align-items-center flex-wrap me-3 mb-5 mb-lg-0">
                 <!--begin::Title-->
-                <h1 class="d-flex align-items-center text-dark fw-bolder fs-3 my-1">Users
+                <h1 class="d-flex align-items-center text-dark fw-bolder fs-3 my-1">{{__('Users')}}
                 <!--begin::Separator-->
                 <span class="h-20px border-gray-200 border-start ms-3 mx-2"></span>
                 <!--end::Separator-->
@@ -23,7 +23,7 @@
     <div class="container">
         <div class="row mb-3">
             <div class="col-md-3">
-                <h2>Create User</h2>
+                <h2>{{__('Create User')}}</h2>
             </div>
         </div>
         <div class="card">
@@ -31,22 +31,22 @@
                 @csrf
                 <div class="p-4">
                     <div class="mb-3">
-                        <label for="username" class="form-label">Name</label>
+                        <label for="username" class="form-label">{{__('Name')}}</label>
                         <input type="text" class="form-control" id="name" name="name" required>
                     </div>
                     <div class="mb-3">
-                        <label for="email" class="form-label">Email address</label>
+                        <label for="email" class="form-label">{{__('Email adress')}}</label>
                         <input type="email" class="form-control" id="email" name="email" required>
                     </div>
                     <div class="mb-3">
-                        <label for="password" class="form-label">Password</label>
+                        <label for="password" class="form-label">{{__('Password')}}</label>
                         <input type="password" class="form-control" id="password" name="password" required>
                     </div>
                     <div class="mb-3">
-                    <label for="confirm-password" class="form-label">Confirm Password</label>
+                    <label for="confirm-password" class="form-label">{{__('Confirm Password')}}</label>
                         <input type="password" class="form-control" id="password_confirmation" name="password_confirmation" required>
                     </div>
-                    <button type="submit" class="btn btn-primary">Create User</button>
+                    <button type="submit" class="btn btn-primary">{{__('Create User')}}</button>
                 </div>
             </form>
         </div>

848cb15ca0d73e5813cf0108e2ed12c3782a13c5
halukbuyukozkan <halukbuyukozkan@gmail.com>
2023-04-20 19:13:38 +0300

    header fixed

--- resources/views/header.blade.php
+++ resources/views/header.blade.php
@@ -41,79 +41,9 @@
                     <div class="menu menu-lg-rounded menu-column menu-lg-row menu-state-bg menu-title-gray-700 menu-state-title-primary menu-state-icon-primary menu-state-bullet-primary menu-arrow-gray-400 fw-bold my-5 my-lg-0 align-items-stretch"
                         id="#kt_header_menu" data-kt-menu="true">
                         <div class="menu-item me-lg-1">
-                            <a class="menu-link active py-3" href="../../demo1/dist/index.html">
+                            <a class="menu-link active py-3" href="{{ route('dashboard') }}">
                                 <span class="menu-title">{{ __('Dashboard') }}</span>
                             </a>
-                        </div>
-                        <div data-kt-menu-trigger="click" data-kt-menu-placement="bottom-start"
-                            class="menu-item menu-lg-down-accordion me-lg-1">
-                            <span class="menu-link py-3">
-                                <span class="menu-title">Crafted</span>
-                                <span class="menu-arrow d-lg-none"></span>
-                            </span>
-                            <div
-                                class="menu-sub menu-sub-lg-down-accordion menu-sub-lg-dropdown menu-rounded-0 py-lg-4 w-lg-225px">
-                                <div data-kt-menu-trigger="{default:'click', lg: 'hover'}"
-                                    data-kt-menu-placement="right-start" class="menu-item menu-lg-down-accordion">
-                                    <span class="menu-link py-3">
-                                        <span class="menu-icon">
-                                            <!--begin::Svg Icon | path: icons/duotune/ecommerce/ecm007.svg-->
-                                            <span class="svg-icon svg-icon-2">
-                                                <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24"
-                                                    viewBox="0 0 24 24" fill="none">
-                                                    <path
-                                                        d="M21 9V11C21 11.6 20.6 12 20 12H14V8H20C20.6 8 21 8.4 21 9ZM10 8H4C3.4 8 3 8.4 3 9V11C3 11.6 3.4 12 4 12H10V8Z"
-                                                        fill="black" />
-                                                    <path
-                                                        d="M15 2C13.3 2 12 3.3 12 5V8H15C16.7 8 18 6.7 18 5C18 3.3 16.7 2 15 2Z"
-                                                        fill="black" />
-                                                    <path opacity="0.3"
-                                                        d="M9 2C10.7 2 12 3.3 12 5V8H9C7.3 8 6 6.7 6 5C6 3.3 7.3 2 9 2ZM4 12V21C4 21.6 4.4 22 5 22H10V12H4ZM20 12V21C20 21.6 19.6 22 19 22H14V12H20Z"
-                                                        fill="black" />
-                                                </svg>
-                                            </span>
-                                            <!--end::Svg Icon-->
-                                        </span>
-                                        <span class="menu-title">{{ __('Pages') }}</span>
-                                        <span class="menu-arrow"></span>
-                                    </span>
-                                    <div
-                                        class="menu-sub menu-sub-lg-down-accordion menu-sub-lg-dropdown menu-active-bg py-lg-4 w-lg-225px">
-                                        <div data-kt-menu-trigger="{default:'click', lg: 'hover'}"
-                                            data-kt-menu-placement="right-start"
-                                            class="menu-item menu-lg-down-accordion">
-                                            <span class="menu-link py-3">
-                                                <span class="menu-bullet">
-                                                    <span class="bullet bullet-dot"></span>
-                                                </span>
-                                                <span class="menu-title">{{ __('Profile') }}</span>
-                                                <span class="menu-arrow"></span>
-                                            </span>
-                                            <div
-                                                class="menu-sub menu-sub-lg-down-accordion menu-sub-lg-dropdown menu-active-bg py-lg-4 w-lg-225px">
-                                                <div class="menu-item">
-                                                    <a class="menu-link py-3"
-                                                        href="../../demo1/dist/pages/profile/overview.html">
-                                                        <span class="menu-bullet">
-                                                            <span class="bullet bullet-dot"></span>
-                                                        </span>
-                                                        <span class="menu-title">{{ __('Overview') }}</span>
-                                                    </a>
-                                                </div>
-                                                <div class="menu-item">
-                                                    <a class="menu-link py-3"
-                                                        href="../../demo1/dist/pages/profile/projects.html">
-                                                        <span class="menu-bullet">
-                                                            <span class="bullet bullet-dot"></span>
-                                                        </span>
-                                                        <span class="menu-title">{{ __('Projects') }}</span>
-                                                    </a>
-                                                </div>
-                                            </div>
-                                        </div>
-                                    </div>
-                                </div>
-                            </div>
                         </div>
                     </div>
                     <!--end::Menu-->
